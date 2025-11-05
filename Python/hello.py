18fad1f8d8ab125c300a256bf70c2e1b0a0e06cd
Omaima Ali Khan <161125067+Omaima-Ali-Khan@users.noreply.github.com>
2025-11-05 16:52:28 +0500

    Add files via upload

---
+++ Python/hello.py
@@ -0,0 +1,17 @@
+print("Hello World");
+# User's name
+name = input("Enter your name: ")
+
+# Greeting
+print("Hello,", name + "!❄️")
+
+# Weather
+print("Girlieee, It’s chilly outside 🥶")
+
+# Fvrt Drink
+drink = input("Any drink for today? ")
+
+print("Great choiceee");
+
+# Result
+print("Python has officially judged your 2 liner code, 🐍 Case closed.")
