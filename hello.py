f76e334631e5b81aadd6d5e59119ae5ee34011f3
Omaima Ali Khan <omaimaak4443500@gmail.com>
2025-11-05 15:46:10 +0500

    First Python Code

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("Hello World");
+# User's name
+name = input("Enter your name: ")
+
+# Greeting
+print("Hello,", name + "!❄️")
+
+# Asking about experience (Python's way of judging 👀)
+print("Hello " + name + "! Brrr… it’s chilly outside 🥶")
+
+# Now let's see what Python *thinks* about that
+drink = input("Drink for today? ")
+
+
+print("Python has officially judged your 2 liner code, 🐍 Case closed.")
